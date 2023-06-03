af2f9726dd231de9d02897097e756a630e7c123f
Matthew Hope <matt@matthewhope.co.uk>
2023-06-03 10:31:54 +0100

    Listen and serve, handle error

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+)
+
+const (
+	addr string = ":8080"
 )
 
 func main() {
@@ -10,6 +15,12 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/wwwroot/", http.StripPrefix("/wwwroot/", http.FileServer(http.Dir("wwwroot/"))))
 	mux.HandleFunc("/", indexHandler)
+	// Listen and serve on defined port
+	err := http.ListenAndServe(addr, mux)
+	// If there is an error with opening server, log and exit with failure status code
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,10 +31,12 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	// Evaluate request method - only HTTP GET supported for index
 	switch r.Method {
+	// HTTP Get returns the home page and Status 200 - OK
 	case http.MethodGet:
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Success"))
 		return
+	// Any other HTTP method returns Status 400 - Bad Request
 	default:
 		badRequestError := fmt.Sprintf("Only %s HTTP methods allowed at endpoint: %s", http.MethodGet, r.URL.Path)
 		http.Error(w, badRequestError, http.StatusBadRequest)
