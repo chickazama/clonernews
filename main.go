19801a7e0616f0c7ac02d6f3fd2b8d4f4ee79f03
Matthew Hope <matt@matthewhope.co.uk>
2023-06-03 11:19:03 +0100

    Add topstories handler

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/wwwroot/", http.StripPrefix("/wwwroot/", http.FileServer(http.Dir("wwwroot/"))))
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/topstories", topStoriesHandler)
 	// Listen and serve on defined port
 	err := http.ListenAndServe(addr, mux)
 	// If there is an error with opening server, log and exit with failure status code
@@ -40,6 +41,39 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		files := []string{
 			defaultLayoutPath,
 			"./wwwroot/pages/index.html",
+		}
+		tmpl, err := template.ParseFiles(files...)
+		// If there is an error parsing HTML files, return Status 500 - Internal Server Error
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		// If there is an error executing layout template, return Status 500 - Internal Server Error
+		err = tmpl.ExecuteTemplate(w, defaultLayoutTemplate, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		return
+	// Any other HTTP method returns Status 400 - Bad Request
+	default:
+		badRequestError := fmt.Sprintf("Only %s HTTP methods allowed at endpoint: %s", http.MethodGet, r.URL.Path)
+		http.Error(w, badRequestError, http.StatusBadRequest)
+		return
+	}
+}
+
+func topStoriesHandler(w http.ResponseWriter, r *http.Request) {
+	// Evaluate request method - only HTTP GET supported for index
+	switch r.Method {
+	// HTTP Get returns the home page and Status 200 - OK
+	case http.MethodGet:
+		// Parse HTML files to populate template
+		files := []string{
+			defaultLayoutPath,
+			"./wwwroot/pages/topstories.html",
 		}
 		tmpl, err := template.ParseFiles(files...)
 		// If there is an error parsing HTML files, return Status 500 - Internal Server Error
