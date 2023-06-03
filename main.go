9ff63b8d04ef0b3b7b40e2d601b8e45421d7865f
Matthew Hope <matt@matthewhope.co.uk>
2023-06-03 12:00:05 +0100

    Add endpointViewMap

--- main.go
+++ main.go
@@ -11,14 +11,18 @@ const (
 	addr                  string = ":8080"
 	defaultLayoutTemplate string = "layout"
 	defaultLayoutPath     string = "./wwwroot/templates/layout.html"
+	pagesPath             string = "./wwwroot/pages/"
 )
 
+var endpointViewMap map[string]string
+
 func main() {
+	initEndpointViewMap()
 	// Create ServeMux, serve static files (HTML, CSS, JS, etc.) & handle endpoints
 	mux := http.NewServeMux()
 	mux.Handle("/wwwroot/", http.StripPrefix("/wwwroot/", http.FileServer(http.Dir("wwwroot/"))))
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/topstories", topStoriesHandler)
+	// mux.HandleFunc("/topstories", topStoriesHandler)
 	// Listen and serve on defined port
 	err := http.ListenAndServe(addr, mux)
 	// If there is an error with opening server, log and exit with failure status code
@@ -28,10 +32,11 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	// Restrict global pattern to root (index) endpoint
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	fmt.Println(r.URL.Path)
+	view, exists := endpointViewMap[r.URL.Path]
+	if !exists {
+		w.WriteHeader(http.StatusNotFound)
+		view = endpointViewMap["/notfound"]
 	}
 	// Evaluate request method - only HTTP GET supported for index
 	switch r.Method {
@@ -40,7 +45,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		// Parse HTML files to populate template
 		files := []string{
 			defaultLayoutPath,
-			"./wwwroot/pages/index.html",
+			view,
 		}
 		tmpl, err := template.ParseFiles(files...)
 		// If there is an error parsing HTML files, return Status 500 - Internal Server Error
@@ -65,35 +70,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func topStoriesHandler(w http.ResponseWriter, r *http.Request) {
-	// Evaluate request method - only HTTP GET supported for index
-	switch r.Method {
-	// HTTP Get returns the home page and Status 200 - OK
-	case http.MethodGet:
-		// Parse HTML files to populate template
-		files := []string{
-			defaultLayoutPath,
-			"./wwwroot/pages/topstories.html",
-		}
-		tmpl, err := template.ParseFiles(files...)
-		// If there is an error parsing HTML files, return Status 500 - Internal Server Error
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(w, "internal server error", http.StatusInternalServerError)
-			return
-		}
-		// If there is an error executing layout template, return Status 500 - Internal Server Error
-		err = tmpl.ExecuteTemplate(w, defaultLayoutTemplate, nil)
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(w, "internal server error", http.StatusInternalServerError)
-			return
-		}
-		return
-	// Any other HTTP method returns Status 400 - Bad Request
-	default:
-		badRequestError := fmt.Sprintf("Only %s HTTP methods allowed at endpoint: %s", http.MethodGet, r.URL.Path)
-		http.Error(w, badRequestError, http.StatusBadRequest)
-		return
-	}
+func initEndpointViewMap() {
+	endpointViewMap = make(map[string]string)
+	endpointViewMap["/"] = pagesPath + "index.html"
+	endpointViewMap["/topstories"] = pagesPath + "topstories.html"
+	endpointViewMap["/notfound"] = pagesPath + "notfound.html"
 }
