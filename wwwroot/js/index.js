f04315f647230aefff0f6aa22770d3a2fac1b486
Matthew Hope <matt@matthewhope.co.uk>
2023-06-03 12:43:31 +0100

    Test out getItemAsync

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -4,4 +4,6 @@ import * as client from "./client.js";
 window.addEventListener("load", async () => {
     await client.getMaxItemIdAsync();
     console.log(client.maxItemId);
+    let data = await client.getItemAsync(client.maxItemId);
+    console.log(data);
 })
