fa3975a71f7d746f88b8b30fee567ffe6a8abc11
Matthew Hope <matt@matthewhope.co.uk>
2023-06-03 12:49:10 +0100

    Generalise getData

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,5 +1,6 @@
 const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 const maxItemEndpoint = "/maxitem.json";
+const newStoriesEndpoint = "/newstories.json";
 const itemPath = "item/";
 
 export let maxItemId = 0;
@@ -14,6 +15,13 @@ export async function getMaxItemIdAsync() {
 
 export async function getItemAsync(itemId) {
     const url = `${baseUrl}${itemPath}${itemId}.json`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+export async function getNewStoriesIdAsync() {
+    const url = `${baseUrl}${newStoriesEndpoint}`;
     const res = await fetch(url);
     const data = await res.json();
     return data;
