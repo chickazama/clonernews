8167dda418df2d24736ce07c501deb60003765b0
Matthew Hope <matt@matthewhope.co.uk>
2023-06-03 12:52:21 +0100

    Test topStories endpoint

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -3,8 +3,10 @@ import * as client from "./client.js";
 window.addEventListener("load", async () => {
     let maxId = await client.getMaxItemIdAsync();
     console.log(maxId);
-    let data = await client.getNewStoriesIdAsync();
+    let data = await client.getTopStoriesIdAsync();
     console.log(data);
+    // let data = await client.getNewStoriesIdAsync();
+    // console.log(data);
     // let data = await client.getItemAsync(client.maxItemId);
     // console.log(data);
 })
