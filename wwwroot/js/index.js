3e673722b74a2f582f6f1700dc8e03318ac6e605
Matthew Hope <matt@matthewhope.co.uk>
2023-06-03 18:24:15 +0100

    Add initAsync function

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -3,17 +3,28 @@ import * as client from "./client.js";
 const msInterval = 5000;
 
 let currentMaxItemId = 0;
+let currentNewStoriesIds = [];
 
 window.addEventListener("load", async () => {
+    await initAsync();
     setInterval(fixedUpdateAsync, msInterval);
 })
 
+async function initAsync() {
+    currentMaxItemId = await client.getMaxItemIdAsync();
+    currentNewStoriesIds = await client.getNewStoriesIdAsync();
+}
 async function fixedUpdateAsync() {
     let maxItemId = await client.getMaxItemIdAsync();
     if (maxItemId == currentMaxItemId) {
         // console.log("Same ID");
         return;
     }
+    let newStoriesIds = await client.getNewStoriesIdAsync();
+    if (newStoriesIds[0] != currentNewStoriesIds[0]) {
+        
+    }
     currentMaxItemId = maxItemId;
     console.log(`${currentMaxItemId}`);
 }
+
