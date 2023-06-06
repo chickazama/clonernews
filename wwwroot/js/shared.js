5c1dc9714c6ce3916bf84f739791d0b8e57b370a
Matthew Hope <matt@matthewhope.co.uk>
2023-06-06 09:19:32 +0100

    Add comments to shared functions

--- wwwroot/js/shared.js
+++ wwwroot/js/shared.js
@@ -72,40 +72,50 @@ export async function buildPostAsync(id) {
     return div;
 }
 
+// This function takes a parameter corresponding to the ID
+// number of an individual comment item, and constructs the
+// comments to be displayed on the page
 async function buildCommentAsync(commentId, reply = false) {
+    // Guard against null data with retry
     let data = null;
     while (data === null) {
         data = await client.getItemAsync(commentId);
     }
+    // Create a div to store comment content
     let div = document.createElement("div");
     div.classList.add("comment");
+    // If it is a reply to another comment - add reply class as well
     if (reply) {
         div.classList.add("reply");
     }
+    // Add the user who authored the comment
     const user = document.createElement("h3");
     const userText = document.createTextNode(`${data.by}`);
     user.appendChild(userText);
-    
+    // Create a paragraph element to store the comment text
     const comment = document.createElement("p");
     if (data.text) {
+        // Use regex to clear out unwanted HTML tags from comment
+        // Then transform HTML escape characters, and render text
         const cleanText = data.text.replace(/<\/?[^>]+(>|$)/g, "");
         const renderedText = decodeHtml(cleanText);
         const commentText = document.createTextNode(renderedText);
         comment.appendChild(commentText);
     } else {
-        comment.innerText = "nothing";
+        comment.innerText = "[This comment was deleted]";
     }
-
+    // Add the user, time of creation and comment text to div
     div.appendChild(user);
     const postTime = document.createElement("h5");
     const postTimeText = document.createTextNode(buildTimeString(data.time));
     postTime.appendChild(postTimeText);
     div.appendChild(postTime);
     div.appendChild(comment);
-    
+    // If there are no replies (i.e. the thread ends here), we are done
     if (!data.kids) {
         return div;
     }
+    // Create span element to act as dropdown to show/hide replies
     const repliesDropSpan = document.createElement("span");
     repliesDropSpan.classList.add("dropdown");
     const repliesHeading = document.createElement("h5");
@@ -114,6 +124,7 @@ async function buildCommentAsync(commentId, reply = false) {
     repliesDropSpan.appendChild(repliesHeading);
     div.appendChild(repliesDropSpan);
     let replies;
+    // Add event listener to dropdown to dynamically show/hide replies
     repliesDropSpan.addEventListener("click", async () => {
         if (!repliesDropSpan.classList.contains("opened")) {
             repliesDropSpan.classList.add("opened");
@@ -139,12 +150,16 @@ async function buildCommentAsync(commentId, reply = false) {
     return div;
 }
 
+// Takes in text containing HTML escape characters, safely converting
+// them into plain-text without danger of XSS attacks
 function decodeHtml(html) {
     const textarea = document.createElement("textarea");
     textarea.innerHTML = html;
     return textarea.value;
 }
 
+// Receives the creation time of an item in UNIX time (seconds since 1/1/1970)
+// and transforms it into a string to be displayed on the web page
 function buildTimeString(unixTime) {
     const cTime = new Date(unixTime*1000);
     const dateStr = `${cTime.getUTCDate()}/${cTime.getUTCMonth()}/${cTime.getUTCFullYear()}`;
